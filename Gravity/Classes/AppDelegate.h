7ca73c820c6893b9b0d032f4c064609aea1181a1
fisch0920 <fisch0920@gmail.com>
2014-03-14 15:46:01 -0400

    Initial Commit

---
+++ Gravity/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Gravity
+//
+//  Created by Travis Fischer on 3/14/14.
+//  Copyright Transitive Bullshit 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
